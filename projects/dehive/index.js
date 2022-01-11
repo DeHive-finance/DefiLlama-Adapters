98f704907a7b6754a95c4c4bb77fc7e87eab3525
newcodepuser <svyatoslav.d@blaize.tech>
2022-01-11 11:43:46 +0200

    remove debug stub

--- projects/dehive/index.js
+++ projects/dehive/index.js
@@ -159,7 +159,7 @@ async function chainTvl(chain, chainBlocks) {
                 console.log('staking', JSON.stringify(staking, null,4));
                 break;
         }
-        const tvls = await stakingTvlFunction(chain, staking.meta, block).catch(()=>{console.log(chain, staking);return []});
+        const tvls = await stakingTvlFunction(chain, staking.meta, block);
         if (typeof tvls === 'string') {
             sdk.util.sumSingleBalance(tvl, transform(staking.meta.tokenAddress), tvls)
         } else {
